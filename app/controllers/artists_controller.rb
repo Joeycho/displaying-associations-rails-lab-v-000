7df06ef03ae220c4c298fa8e9d634b3c15be2bb7
Joey Cho <cjy12182000@naver.com>
2019-02-22 09:02:23 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,7 @@
 class ArtistsController < ApplicationController
+  
+  before_action :set_artist!, only: [:show, :edit, :update]
+
   def index
   end
 
@@ -46,5 +49,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def set_artist!
+    @artist = Artist.find(params[:id])
   end
 end
